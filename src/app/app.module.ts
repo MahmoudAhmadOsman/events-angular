21e828ac73483c0d94ab1e4382d3ffe43087f3ff
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-05-21 14:01:51 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { PostEventComponent } from './components/post-event/post-event.component';
+import { FeaturedEventsComponent } from './components/featured-events/featured-events.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    PostEventComponent,
+    FeaturedEventsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/featured-events/featured-events.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-events',
+  templateUrl: './featured-events.component.html',
+  styleUrls: ['./featured-events.component.scss']
+})
+export class FeaturedEventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
