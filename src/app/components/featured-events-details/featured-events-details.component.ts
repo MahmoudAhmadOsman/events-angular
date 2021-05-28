d0d6b2d0b5beca00062420c3cd8272ce60f16e05
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-05-28 03:12:39 -0500

    modified featured-events component

--- src/app/components/featured-events-details/featured-events-details.component.ts
+++ src/app/components/featured-events-details/featured-events-details.component.ts
@@ -10,7 +10,7 @@ import { ActivatedRoute } from '@angular/router';
 export class FeaturedEventsDetailsComponent implements OnInit {
 
   public eventDetails: any;
-
+ 
   constructor(private eventsService: EventsService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
