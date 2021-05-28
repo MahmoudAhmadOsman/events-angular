2299a11dad1a5a0807553bdf5ea33b76296f1d46
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-05-27 21:21:41 -0500

    added fa heart

--- src/app/models/event.ts
+++ src/app/models/event.ts
@@ -3,10 +3,13 @@ export class Event {
     title: string;
     imageURL: string;
     price: number;
+    soldOut: boolean;
     seats: number;
+    summary: string;
     description: string;
     city: string;
     state: string;
+    zipCode: number;
     location: string;
-    createdAt: string
+    createdAt: Date;
 }
---
+++ src/assets/js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready( ()=> {
+    $(".featured-events .card .heart-on-card .fa-heart").click(() => {
+        $(this).addClass("fas fa-heart");
+        // $(this).hide(100);
+        
+    });
+});
+  
+ 
